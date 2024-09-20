adadde6f8e0b14a6c183df1f51a9f1a957897967
AlexanderGoose <alexandergoose97@gmail.com>
2024-09-20 09:41:49 -0600

    testing commiting through terminal

---
+++ conditionals.R
@@ -0,0 +1,12 @@
+# CONDITIONALS
+
+x <- 10
+
+if (x == 10){
+  # code
+  print('x is equal to 10')
+} else if (x == 12) {
+  print('x is equal to 12') 
+} else {
+  print('x was not equal to 10 or 12')
+}
