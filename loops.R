01edfea5f957663780c9522cc44eede5e9bc77fc
AlexanderGoose <alexandergoose97@gmail.com>
2024-09-20 09:49:10 -0600

    adding syntax file for basic loops

---
+++ loops.R
@@ -0,0 +1,17 @@
+# LOOPS 
+
+# WHILE LOOPS
+x <- 10
+
+while (x < 12) {
+  print('x too small, increasing!')
+  x <- x + 1
+  print(x)
+}
+
+# FOR LOOPS
+lst <- c(1,2,3)
+
+for (i in lst) {
+  print(i)
+}
